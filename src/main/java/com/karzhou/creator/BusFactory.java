4bf79b26cfd9dc070b61d80ec2802b7ffa0babc2
Aliaksei Karzhou <alexkorzhwork@gmail.com>
2025-03-04 22:35:00 +0300

    Add logs

--- src/main/java/com/karzhou/creator/BusFactory.java
+++ src/main/java/com/karzhou/creator/BusFactory.java
@@ -2,10 +2,14 @@ package com.karzhou.creator;
 
 import com.karzhou.entity.Bus;
 import com.karzhou.validator.BusValidator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.Date;
 
 public class BusFactory {
+
+    private static final Logger logger = LogManager.getLogger(BusFactory.class);
 
     private final BusValidator busValidator;
 
@@ -21,10 +25,20 @@ public class BusFactory {
                          Date startOfOperation,
                          float mileage) {
 
+        logger.debug("Creating a new Bus instance with driverName: {}, driverSurname: {}, busNumber: {}, trail: {}, busBrand: {}, startOfOperation: {}, mileage: {}",
+                driverName, driverSurname, busNumber, trail, busBrand, startOfOperation, mileage);
+
         Bus bus = new Bus(driverName, driverSurname, busNumber, trail, busBrand, startOfOperation, mileage);
 
-        busValidator.validate(bus);
+        try {
+            busValidator.validate(bus);
+            logger.info("Bus validation successful for busNumber: {}", busNumber);
+        } catch (Exception e) {
+            logger.error("Bus validation failed for busNumber: {}", busNumber, e);
+            throw e;
+        }
 
+        logger.info("Bus created successfully: {}", bus);
         return bus;
     }
 }
--- src/main/java/com/karzhou/service/BusService.java
+++ src/main/java/com/karzhou/service/BusService.java
@@ -1,6 +1,8 @@
 package com.karzhou.service;
 
 import com.karzhou.entity.Bus;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -8,17 +10,25 @@ import java.util.List;
 
 public class BusService {
 
+    private static final Logger logger = LogManager.getLogger(BusService.class);
+
     public List<Bus> getBusesByTrail(List<Bus> buses, String trail) {
+        logger.debug("Searching buses by trail: {}", trail);
+
         List<Bus> result = new ArrayList<>();
         for (Bus bus : buses) {
             if (bus.getTrail().equals(trail)) {
                 result.add(bus);
             }
         }
+
+        logger.info("Found {} buses for trail: {}", result.size(), trail);
         return result;
     }
 
     public List<Bus> getBusesByOperationPeriod(List<Bus> buses, int years) {
+        logger.debug("Searching buses by operation period (more than {} years)", years);
+
         List<Bus> result = new ArrayList<>();
         Date currentDate = new Date();
         for (Bus bus : buses) {
@@ -28,16 +38,22 @@ public class BusService {
                 result.add(bus);
             }
         }
+
+        logger.info("Found {} buses with operation period more than {} years", result.size(), years);
         return result;
     }
 
     public List<Bus> getBusesByMileage(List<Bus> buses, float mileage) {
+        logger.debug("Searching buses by mileage (more than {} km)", mileage);
+
         List<Bus> result = new ArrayList<>();
         for (Bus bus : buses) {
             if (bus.getMileage() > mileage) {
                 result.add(bus);
             }
         }
+
+        logger.info("Found {} buses with mileage more than {} km", result.size(), mileage);
         return result;
     }
 }
