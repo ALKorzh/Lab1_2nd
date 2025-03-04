1b825e99c95d9e204e85e7a3f39fdba35c6a9bf0
Aliaksei Karzhou <alexkorzhwork@gmail.com>
2025-03-04 21:54:12 +0300

    Add BusValidator

---
+++ src/main/java/com/karzhou/factory/BusFactory.java
@@ -0,0 +1,30 @@
+package com.karzhou.factory;
+
+import com.karzhou.entity.Bus;
+import com.karzhou.validator.BusValidator;
+
+import java.util.Date;
+
+public class BusFactory {
+
+    private final BusValidator busValidator;
+
+    public BusFactory(BusValidator busValidator) {
+        this.busValidator = busValidator;
+    }
+
+    public Bus createBus(String driverName,
+                         String driverSurname,
+                         String busNumber,
+                         String trail,
+                         String busBrand,
+                         Date startOfOperation,
+                         float mileage) {
+
+        Bus bus = new Bus(driverName, driverSurname, busNumber, trail, busBrand, startOfOperation, mileage);
+
+        busValidator.validate(bus);
+
+        return bus;
+    }
+}
